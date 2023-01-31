d3076409610aa9208f23317755009d0289dbebda
Braxton Diaz <braxton.diaz@outlook.com>
2023-01-30 19:04:47 -0800

    clear image files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
-import React from "react"
-import HeroImage from "../assets/heroImage.png"
-import {MdOutlineKeyboardArrowRight } from "react-icons/md"
-import {Link} from "react-scroll"
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Home = () => {
-  return(
-    <div 
-      name="Home" 
+  return (
+    <div
+      name="Home"
       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div
@@ -14,31 +14,33 @@ const Home = () => {
                    justify-center h-full px-4 md:flex-row"
       >
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white"> 
-            I'm a Full stack dev 
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a Full stack dev
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 10 years of experience writing software!
-            Currently a senior studying computer sciece
-            Enjoy my portfolio!
+            I have 10 years of experience writing software! Currently a senior
+            studying computer sciece Enjoy my portfolio!
           </p>
 
           <div>
-            <Link 
+            <Link
               to="Portfolio"
-              smooth duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
-                <MdOutlineKeyboardArrowRight size={25}
-                className="ml-1" />
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </Link>
           </div>
         </div>
 
         <div>
-          <img src={HeroImage} alt="My Portfolio Pic"
+          <img
+            src={HeroImage}
+            alt="My Portfolio Pic"
             className="rounded-2xl mx-auto w-2/3 md:w-full"
           />
         </div>
